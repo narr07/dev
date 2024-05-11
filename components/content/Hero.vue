3a241279e0fddd655407992121fdd597d07d5971
Dinar, Permadi <dinarpermadi07@gmail.com>
2024-05-12 03:06:26 +0700

    refactor: Update Hero.vue to add user, desain, and teknologi tabs

--- components/content/Hero.vue
+++ components/content/Hero.vue
@@ -24,23 +24,26 @@ onMounted(() => {
 })
 
 const tabitems = [{
-  label: 'Pendidikan',
-  icon: 'i-ph-graduation-cap-duotone',
-  content: 'Dalam ranah pendidikan, saya menghadirkan informasi terkini seputar perkembangan dunia pendidikan, metode pembelajaran inovatif, dan berbagai artikel inspiratif untuk membantu Anda memahami dan merencanakan perjalanan pendidikan yang sukses.',
+  label: 'Hallo 😄',
+  slot: 'user',
+  icon: 'i-ph-user-duotone',
+
 }, {
   label: 'Desain',
+  slot: 'desain',
   icon: 'i-ph-palette-duotone',
-  content: 'Jelajahi keindahan dunia seni melalui artikel-artikel yang membahas beragam bentuk seni, termasuk seni visual, musik, teater, dan lebih banyak lagi. Dapatkan wawasan mendalam tentang kreativitas, teknik seni terkini, serta inspirasi untuk memperkaya pengalaman seni Anda.',
+
 }, {
   label: 'Teknologi',
+  slot: 'teknologi',
   icon: 'i-ph-code-duotone',
-  content: 'Melibatkan diri dalam dunia teknologi yang cepat berubah dengan mengikuti berita terbaru, tren terkini, dan perkembangan terkini di bidang teknologi. Temukan ulasan perangkat lunak, panduan pengembangan, dan artikel informatif lainnya.',
+
 }]
 </script>
 
 <template>
   <UContainer>
-    <div class="grid md:grid-cols-2 gap-4">
+    <div class="grid md:grid-cols-2 h-full gap-4">
       <UCard class="cardHover">
         <UCarousel
           ref="carouselRef"
@@ -59,7 +62,10 @@ const tabitems = [{
         </UCarousel>
       </UCard>
 
-      <UTabs :items="tabitems">
+      <UTabs
+        class="h-full"
+        :items="tabitems"
+      >
         <template #default="{ item }">
           <div class="flex items-center gap-2 relative truncate">
             <UIcon
@@ -71,17 +77,28 @@ const tabitems = [{
             </span>
           </div>
         </template>
-        <template #item="{ item }">
-          <UCard class="cardHover">
+        <template #user>
+          <UCard class="cardHover h-full">
+            <p>
+              Saya <span class="text-permadi-950 dark:text-yellow font-bold">
+
+                Dinar Permadi Yusup,
+              </span>
+              Seorang Guru, Desainer, dan Programmer
+            </p>
+          </UCard>
+        </template>
+        <template #desain>
+          <UCard class="cardHover h-full">
             <div class=" p-2 items-center gap-4 md:gap-8">
-              <div class="flex flex-col justify-center pb-4 md:pb-0 w-full">
-                <h2 class="font-title">
-                  {{ item.label }}
-                </h2>
-                <p class="mt-1 md:text-left">
-                  {{ item.content }}
-                </p>
-              </div>
+              <div class="flex flex-col h-full justify-center pb-4 md:pb-0 w-full" />
+            </div>
+          </UCard>
+        </template>
+        <template #teknologi>
+          <UCard class="cardHover h-full">
+            <div class=" p-2 items-center gap-4 md:gap-8">
+              <div class="flex flex-col h-full justify-center pb-4 md:pb-0 w-full" />
             </div>
           </UCard>
         </template>
